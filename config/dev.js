f06a481ced3cfe3c4330be650b1507e8211a95c2
Manik Bhardwaj <manik782@gmail.com>
2026-02-20 20:55:37 +0530

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+//dev.js don't commit this file
+module.exports = {
+    googleClientID: '760215075604-c9df0m040pl5332pbdefnlnvda33c800.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://manik782_db_user:GQP9e5GiZnalO3gd@cluster0.eou0pbz.mongodb.net/manikdb?appName=Cluster0',
+    cookieKey: 'jlkjfdklajfdajiejijijfdf',
+};
+
+
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+//const authRoutes = require('./routes/authRoutes');
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]); //to fix connection issues
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'});
-});
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
